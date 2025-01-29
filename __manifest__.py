ae84b5b5482cd03f2b1e894837e1282ecc0ad83c
AlfaSystemas5457 <software@alfasystemas.com.mx>
2025-01-29 14:10:26 -0600

    add link

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Cuenta analitica con stock',
     'version': '0.1',
     'author': 'DGV',
-    # 'website': 'https://www.ganemo.co',
+    'website': 'https://github.com/AlfaSystemas5457/account_analytic_with_stock',
     'summary': 'Permite establecer relación entre los movimientos de stock y la cuentas analiticas.',
     'category': 'Accounting',
     'depends': ['stock_account', 'stock'],
